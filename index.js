64fcf19eace234f09881281b91ba3e58e0ab5206
WorstWorld2 <koutouris@gmail.com>
2025-08-05 14:47:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import { Client, GatewayIntentBits, Events } from 'discord.js';
+import { Client, GatewayIntentBits, Events, REST, Routes, SlashCommandBuilder } from 'discord.js';
 
 const client = new Client({
     intents: [
@@ -13,8 +13,28 @@ const client = new Client({
     ]
 });
 
-client.once(Events.ClientReady, () => {
+async function registerAboutCommand() {
+    const command = new SlashCommandBuilder()
+        .setName('about')
+        .setDescription('Shows information about NickBot')
+        .toJSON();
+
+    const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+    try {
+        await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            { body: [command] }
+        );
+        console.log('Slash command /about registered.');
+    } catch (error) {
+        console.error('Error registering /about command:', error);
+    }
+}
+
+client.once(Events.ClientReady, async () => {
     console.log(`Logged in as ${client.user.tag}`);
+    await registerAboutCommand();
 });
 
 client.on(Events.MessageCreate, message => {
@@ -22,6 +42,14 @@ client.on(Events.MessageCreate, message => {
 
     if (message.content === 'Hi NickBot') {
         message.reply(`Hey ${message.author.username}!`);
+    }
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === 'about') {
+        await interaction.reply('My name is NickBot! I was created by Nick12.');
     }
 });
 
